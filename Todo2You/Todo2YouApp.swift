dd04e6d35518e28ad8a38681cfac615c6c0721b7
Gabriel <gkustodor@gmail.com>
2022-01-07 08:25:59 -0300

    Initial Commit

---
+++ Todo2You/Todo2YouApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo2YouApp.swift
+//  Todo2You
+//
+//  Created by Gabriel Custódio on 07/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo2YouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
